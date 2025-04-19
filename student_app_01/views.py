d7820a03bbe883afd95313bd093432c710d460bd
Rohit Patel <RP380972@gmail.com>
2025-04-19 08:19:20 +0530

    Update views.py

--- student_app_01/views.py
+++ student_app_01/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 from django.template.loader import render_to_string
 from weasyprint import HTML
 from django.conf import settings
+from django.templatetags.static import static
 import os
 
 def student_create(request):
@@ -24,17 +25,21 @@ def generate_pdf(request,student):
     logo_abspath = os.path.join(settings.BASE_DIR, 'student_app_01/static/images/andc_logo_01.png')
     logo_uri = 'file:///' + logo_abspath.replace('\\', '/')
 
+    photo_url = request.build_absolute_uri(student.photo.url)
+    signature_url = request.build_absolute_uri(student.signature.url)
+
     # Pass this to the template
     html_string = render_to_string('pdf_01.html', {
         'student': student,
         'logo_path': logo_uri,
-        'request': request
+        'photo_url': photo_url,
+        'signature_url': signature_url
     })
     # print(html_string) 
     # //locally
     # html = HTML(string=html_string, base_url="http://127.0.0.1:8000/")
     # live render
-    html = HTML(string=html_string, base_url=request.build_absolute_uri())
+    html = HTML(string=html_string, base_url=request.build_absolute_uri('/'))
     pdf = html.write_pdf()
     response = HttpResponse(pdf, content_type='application/pdf')
     response['Content-Disposition'] = f'attachment; filename="{student.student_name}_details.pdf"'
